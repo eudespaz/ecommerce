5b4fa5d6da474d8e2281c1967820c221f6c64118
Eudes Paz <eudespaz@live.com>
2020-08-29 15:51:06 -0300

    CRIAMOS A CLASSE PAGE

--- index.php
+++ index.php
@@ -2,17 +2,18 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
 
-	$resultados = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
 
-	echo json_encode($resultados);
+	$page->setTpl("index");
 
 });
 
---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,7 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html>
+<head>
+	<title>Hcode Store</title>
+	<meta charset="utf-8">
+</head>
+<body>
